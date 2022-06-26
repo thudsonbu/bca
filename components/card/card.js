3c49dc896ece7d5af7a33aa5135db26a71f1f03f
zbarnz <zbarnz@yahoo.com>
2022-06-26 12:45:12 -0600

    grid-cards

--- components/card/card.js
+++ components/card/card.js
@@ -1,9 +1,17 @@
 import styles from "./card.module.scss";
+import Image from "next/image";
+import placeholder from "../../public/868690.png";
 
-export default function Card({ number, title }) {
+export default function Card({ title, description, img }) {
   return (
     <div className={styles.container}>
+      {img && (
+        <div className={styles.imageContainer}>
+          <Image layout="fill" src={placeholder} />
+        </div>
+      )}
       <h1>{title}</h1>
+      {description && <p>{description}</p>}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,26 @@
 import Nav from "../components/nav/nav";
 import Jumbotron from "../components/jumbotron/jumbotron";
 import Card from "../components/card/card";
+import CardGroup from "../components/card-group/card-group"
+
+import styles from '../styles/home.module.scss'
 
 const Home = () => {
   return (
     <div>
       <Nav />
       <Jumbotron />
-      <div>
+      <div className={styles.stats}>
         <Card />
         <Card />
         <Card />
       </div>
+      <CardGroup columns={4}>
+        <Card img={true} description='This is a description about whatever'/>
+        <Card img={true} description='This is a description about whatever'/>
+        <Card img={true} description='This is a description about whatever'/>
+        <Card img={true} description='This is a description about whatever'/>
+      </CardGroup>
     </div>
   );
 };
