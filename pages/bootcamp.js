f20747ab942af590c014980c1cb00f93a57679d4
thudsonbu <54583311+thudsonbu@users.noreply.github.com>
2022-08-24 21:03:45 -0400

    chore(layouts): add layouts generic styles module

--- pages/bootcamp.js
+++ pages/bootcamp.js
@@ -69,9 +69,6 @@ const Bootcamp = () => {
           }
         />
       </CardGrid>
-
-
-
     </>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import Jumbotron from "../components/jumbotron/jumbotron";
-import ImageStatement from "../components/image-statement/image-statement";
-import SectionTitle from "../components/section-title/section-title";
+import Jumbotron       from "../components/jumbotron/jumbotron";
+import ImageStatement  from "../components/image-statement/image-statement";
+import SectionTitle    from "../components/section-title/section-title";
 import SectionSubtitle from "../components/section-subtitle/section-subtitle";
-import ImageGrid from "../components/image-grid/image-grid";
-import Partners from "../components/partners/partners";
-import ImageCard from "../components/image-card/image-card";
-import styles from "../styles/home.module.scss";
+import ImageGrid       from "../components/image-grid/image-grid";
+import Partners        from "../components/partners/partners";
+import ImageCard       from "../components/image-card/image-card";
+
+import home_styles   from "../styles/home.module.scss";
+import layout_styles from "../styles/layouts.module.scss";
 
 const Home = () => {
   return (
@@ -13,7 +15,7 @@ const Home = () => {
       <Jumbotron
         title={"Bentley Consulting Association"}
       />
-      <div className={styles.padding}>
+      <div className={layout_styles.section}>
         <ImageStatement
           src="./images/BCALogo.png"
           title={"About"}
@@ -23,44 +25,46 @@ const Home = () => {
         />
       </div>
       <SectionTitle title={"What We Do"} background={"blue"} />
-      <ImageGrid
-        items={[
-          {
-            src: "./images/education.jpg",
-            title: "Real Experience, Real Skills",
-            description:
-              "Our members reinforce concepts taught through our Bootcamp and Associate Consultant programming by working on actual client projects. These engagements vary across business segments, allowing our members to develop diverse technical skills along with important soft skills needed to be successful in consulting. These include presentation skills, professionalism, and many more!",
-            link: "./students",
-            link_text: "Learn more",
-          },
-          {
-            src: "./images/charts.jpg",
-            title: "Consulting Bootcamps",
-            description:
-              "Our 5-week Bootcamp serves as the primary source of entry for the organization. In the BCA Bootcamp, students learn fundamental skills necessary for completing the consulting process. This program culminates in a client project where members apply their skills to develop real world experience.",
-            link: "./students",
-            link_text: "Learn more",
-          },
-          {
-            src: "./images/events.jpg",
-            title: "Case Competitions",
-            description:
-              "BCA hosts a case competition each semester: freshman case competition in the fall, and an intercollegiate competition in the spring. These case competitions give participants valuable experience in solving complex business problems and creating final presentations for a panel of judges working in consulting.",
-            link: "./students",
-            link_text: "Learn more",
-          },
-          {
-            src: "./images/team.jpg",
-            title: "Client Projects",
-            description:
-              "As both a Bootcamp Participant and an Associate Consultant, members have the opportunity to work with real life businesses to complete probono consulting projects. These engagements allow our members to apply skills taught by BCA and expand their knowledge in a variety of business segments. ",
-            link: "./clients",
-            link_text: "Learn more",
-          },
-        ]}
-      />
+      <div className={layout_styles.section}>
+        <ImageGrid
+          items={[
+            {
+              src: "./images/education.jpg",
+              title: "Real Experience, Real Skills",
+              description:
+                "Our members reinforce concepts taught through our Bootcamp and Associate Consultant programming by working on actual client projects. These engagements vary across business segments, allowing our members to develop diverse technical skills along with important soft skills needed to be successful in consulting. These include presentation skills, professionalism, and many more!",
+              link: "./students",
+              link_text: "Learn more",
+            },
+            {
+              src: "./images/charts.jpg",
+              title: "Consulting Bootcamps",
+              description:
+                "Our 5-week Bootcamp serves as the primary source of entry for the organization. In the BCA Bootcamp, students learn fundamental skills necessary for completing the consulting process. This program culminates in a client project where members apply their skills to develop real world experience.",
+              link: "./students",
+              link_text: "Learn more",
+            },
+            {
+              src: "./images/events.jpg",
+              title: "Case Competitions",
+              description:
+                "BCA hosts a case competition each semester: freshman case competition in the fall, and an intercollegiate competition in the spring. These case competitions give participants valuable experience in solving complex business problems and creating final presentations for a panel of judges working in consulting.",
+              link: "./students",
+              link_text: "Learn more",
+            },
+            {
+              src: "./images/team.jpg",
+              title: "Client Projects",
+              description:
+                "As both a Bootcamp Participant and an Associate Consultant, members have the opportunity to work with real life businesses to complete probono consulting projects. These engagements allow our members to apply skills taught by BCA and expand their knowledge in a variety of business segments. ",
+              link: "./clients",
+              link_text: "Learn more",
+            },
+          ]}
+        />
+      </div>
       <SectionTitle title={"Our Partners"} background={"blue"} />
-      <div className={styles.padding}>
+      <div className={layout_styles.section}>
         <Partners
           partners={[
             { src: "./images/slalom.png", style: "slalom" },
@@ -72,24 +76,26 @@ const Home = () => {
         />
       </div>
       <SectionTitle title={"Learn More"} background={"blue"} />
-      <SectionSubtitle title={"Meet our team or see our upcoming events"} />
-      <div className={styles.flex}>
-        <ImageCard
-          title={"Our Team"}
-          description={
-            "Meet our executive board and consultants or reach out directly."
-          }
-          src={"./images/team.jpg"}
-          href={"./team"}
-        />
-        <ImageCard
-          title={"Events"}
-          description={"See what events the BCA has planned this semester"}
-          src={"./images/events.jpg"}
-          href={"./events"}
-        />
+      <div className={layout_styles.section}>
+        <SectionSubtitle title={"Meet our team or see our upcoming events"} />
+        <div className={layout_styles.flex}>
+          <ImageCard
+            title={"Our Team"}
+            description={
+              "Meet our executive board and consultants or reach out directly."
+            }
+            src={"./images/team.jpg"}
+            href={"./team"}
+          />
+          <ImageCard
+            title={"Events"}
+            description={"See what events the BCA has planned this semester"}
+            src={"./images/events.jpg"}
+            href={"./events"}
+          />
+        </div>
       </div>
-      <div className={styles.padding}>
+      <div className={layout_styles.section}>
         <SectionSubtitle title={"Reach out to us"} />
         <ImageGrid
           items={[
